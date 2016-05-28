a4c92ed907e39e3ea70f691976d5d7fa677c8f29
Priyank <prmathur@adobe.com>
2016-05-28 15:12:21 -0700

    added model saver + t-board

--- joint.py
+++ joint.py
@@ -12,18 +12,20 @@ from six.moves import range
 import tensorflow as tf
 import numpy as np
 import pandas as pd
+import os
 
 tf.flags.DEFINE_float("learning_rate", 0.01, "Learning rate for Adam Optimizer.")
 tf.flags.DEFINE_float("epsilon", 1e-8, "Epsilon value for Adam Optimizer.")
+tf.flags.DEFINE_float("regularization", 0.02, "Regularization.")
 tf.flags.DEFINE_float("max_grad_norm", 40.0, "Clip gradients to this norm.")
 tf.flags.DEFINE_integer("evaluation_interval", 10, "Evaluate and print results every x epochs")
 tf.flags.DEFINE_integer("batch_size", 32, "Batch size for training.")
 tf.flags.DEFINE_integer("hops", 3, "Number of hops in the Memory Network.")
-tf.flags.DEFINE_integer("epochs", 200, "Number of epochs to train for.")
+tf.flags.DEFINE_integer("epochs", 2, "Number of epochs to train for.")
 tf.flags.DEFINE_integer("embedding_size", 40, "Embedding size for embedding matrices.")
 tf.flags.DEFINE_integer("memory_size", 50, "Maximum size of memory.")
 tf.flags.DEFINE_integer("random_state", None, "Random state.")
-tf.flags.DEFINE_string("data_dir", "../code/data/babi/tasks_1-20_v1-2/en/", "Directory containing bAbI tasks")
+tf.flags.DEFINE_string("data_dir", "babi/tasks_1-20_v1-2/en/", "Directory containing bAbI tasks")
 tf.flags.DEFINE_string("output_file", "scores.csv", "Name of output file for final bAbI accuracy scores.")
 FLAGS = tf.flags.FLAGS
 
@@ -37,6 +39,41 @@ for i in ids:
     train.append(tr)
     test.append(te)
 data = list(chain.from_iterable(train + test))
+
+def get_log_dir_name():
+    lr = FLAGS.learning_rate
+    eps = FLAGS.epsilon
+    mgn = FLAGS.max_grad_norm
+    hp = FLAGS.hops
+    es = FLAGS.embedding_size
+    ms = FLAGS.memory_size
+    # ti = FLAGS.task_id
+    reg = FLAGS.regularization
+
+    log_dir_name = "lr={0}_eps={1}_mgn={2}_hp={3}_es={4}_ms={5}_reg={6}".format(lr, eps, mgn, hp, es, ms, reg)
+    return os.path.join('./logs/joint/', log_dir_name)
+
+def get_wt_dir_name():
+    lr = FLAGS.learning_rate
+    eps = FLAGS.epsilon
+    mgn = FLAGS.max_grad_norm
+    hp = FLAGS.hops
+    es = FLAGS.embedding_size
+    ms = FLAGS.memory_size
+    # ti = FLAGS.task_id
+    reg = FLAGS.regularization
+
+    log_dir_name = "lr={0}_eps={1}_mgn={2}_hp={3}_es={4}_ms={5}_reg={6}".format(lr, eps, mgn, hp, es, ms, reg)
+    return os.path.join('./weights', log_dir_name)
+
+def gen_writers(sess, base_dir):
+    writers = {}
+    writers["loss"] = tf.train.SummaryWriter(os.path.join(base_dir, "loss") , sess.graph)
+
+    for i in range(1, 21):
+        writers["task{0}".format(i)] = tf.train.SummaryWriter(os.path.join(base_dir, "task{0}".format(i)), sess.graph)
+
+    return writers
 
 vocab = sorted(reduce(lambda x, y: x | y, (set(list(chain.from_iterable(s)) + q + a) for s, q, a in data)))
 word_idx = dict((c, i + 1) for i, c in enumerate(vocab))
@@ -97,13 +134,18 @@ val_labels = np.argmax(valA, axis=1)
 
 tf.set_random_seed(FLAGS.random_state)
 batch_size = FLAGS.batch_size
-optimizer = tf.train.AdamOptimizer(learning_rate=FLAGS.learning_rate, epsilon=FLAGS.epsilon)
+# optimizer = tf.train.AdamOptimizer(learning_rate=FLAGS.learning_rate, epsilon=FLAGS.epsilon)
 
 # This avoids feeding 1 task after another, instead each batch has a random sampling of tasks
 batches = zip(range(0, n_train-batch_size, batch_size), range(batch_size, n_train, batch_size))
 with tf.Session() as sess:
+    print(batch_size, vocab_size, sentence_size, memory_size, FLAGS.embedding_size, FLAGS.hops, FLAGS.max_grad_norm, FLAGS.regularization, FLAGS.learning_rate, FLAGS.epsilon)
+
     model = MemN2N(batch_size, vocab_size, sentence_size, memory_size, FLAGS.embedding_size, session=sess,
-                   hops=FLAGS.hops, max_grad_norm=FLAGS.max_grad_norm, optimizer=optimizer)
+                   hops=FLAGS.hops, max_grad_norm=FLAGS.max_grad_norm, l2=FLAGS.regularization, lr=FLAGS.learning_rate, epsilon=FLAGS.epsilon)
+
+    writers = gen_writers(sess, get_log_dir_name())
+
     for i in range(1, FLAGS.epochs+1):
         np.random.shuffle(batches)
         total_cost = 0.0
@@ -114,6 +156,10 @@ with tf.Session() as sess:
             a = trainA[start:end]
             cost_t, cost_t_summary, cost_ema = model.batch_fit(s, q, a)
             total_cost += cost_t
+
+        total_cost_summary = tf.scalar_summary("epoch_loss", total_cost)
+        tcs = sess.run(total_cost_summary)
+        writers["loss"].add_summary(tcs, i)
 
         if i % FLAGS.evaluation_interval == 0:
             train_accs = []
@@ -154,11 +200,23 @@ with tf.Session() as sess:
                 print("Validation Accuracy = {}".format(t2))
                 print("Testing Accuracy = {}".format(t3))
                 print()
+
+                train_acc_summary = tf.scalar_summary("train_acc", t1)
+                val_acc_summary = tf.scalar_summary("val_acc", t2)
+
+                vas = sess.run(val_acc_summary)
+                tas = sess.run(train_acc_summary)
+
+                writers["task{0}".format(t)].add_summary(vas, i)
+                writers["task{0}".format(t)].add_summary(tas, i)
+
                 t += 1
             print('-----------------------')
 
         # Write final results to csv file
         if i == FLAGS.epochs:
+            model.save_model(get_wt_dir_name())
+            print(sess.run(model.A))
             print('Writing final results to {}'.format(FLAGS.output_file))
             df = pd.DataFrame({
             'Training Accuracy': train_accs,
