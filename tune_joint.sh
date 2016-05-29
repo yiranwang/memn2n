b94f75b8d08bb0451b8500aa892e88b8786cb10b
Eylon Stroh <eylonmaestroh@gmail.com>
2016-05-28 18:40:11 -0700

    Different output files for different values of lambda

--- tune_joint.sh
+++ tune_joint.sh
@@ -1,8 +1,9 @@
 L2=( 0 1e-7 1e-6 1e-5 1e-4 1e-3 1e-2 1e-1 1 )
 
+alpha=0.001
 dir=../cs224d-project/data/babi/tasks_1-20_v1-2/en/
 
 for lamb in "${L2[@]}"; do
     echo tuning $lamb
-    python joint.py --data_dir $dir --early 50 --learning_rate 0.001 --regularization $lamb > joint_alpha_0.001_lambda_$lamb.txt
+    python joint.py --data_dir $dir --output_file scores_alpha_$alpha.lambda_$lamb.csv --early 50 --learning_rate $alpha --regularization $lamb > joint_alpha_0.001_lambda_$lamb.txt
 done
